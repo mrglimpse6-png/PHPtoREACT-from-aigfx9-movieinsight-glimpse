17d9a18bfc2b6acac5eb51fc112de6113c7abef0
movieinsightexplorer-svg <movieinsight.explorer@gmail.com>
2025-10-04 12:38:31 +0500

    Updated BlogForm.tsx

--- src/admin/pages/Blogs/BlogForm.tsx
+++ src/admin/pages/Blogs/BlogForm.tsx
@@ -6,7 +6,7 @@
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Loader2, Upload, X } from 'lucide-react';
+import { Loader as Loader2, Upload, X } from 'lucide-react';
 import { Blog } from '../../utils/api';
 import { blogSchema, BlogFormValues } from '../../utils/validation';
 import { useCreateBlog, useUpdateBlog, useUploadImage } from '../../hooks/useBlogs';
